c9426ae953688f1ad22872032e3dcd01152f9dde
Pranav Vaidik Dhulipala <pranav.vaidik@gmail.com>
2018-12-15 08:09:36 -0800

    cleared more errors.

--- main.py
+++ main.py
@@ -140,8 +140,8 @@ checkpointer = ModelCheckpoint(filepath='weights.best.from_scratch.vanilla.hdf5'
 #model.fit_generator(generator=training_generator, validation_data=validation_generator, epochs= epochs, callbacks=[checkpointer], use_multiprocessing=True, workers=6)
 
 #direct_train
-model.fit(train_tensors, train_targets, 
-          validation_data=(valid_tensors, valid_targets),
+model.fit(train_tensors, targets_train, 
+          validation_data=(valid_tensors, targets_val),
           epochs=epochs, batch_size=20, callbacks=[checkpointer], verbose=1)
 
 #load model with best validation loss
@@ -151,7 +151,7 @@ model.load_weights('saved_models/weights.best.from_scratch.vanilla.hdf5')
 dog_breed_predictions = [np.argmax(model.predict(np.expand_dims(tensor, axis=0))) for tensor in test_tensors]
 
 # report test accuracy
-test_accuracy = 100*np.sum(np.array(dog_breed_predictions)==np.argmax(test_targets, axis=1))/len(dog_breed_predictions)
+test_accuracy = 100*np.sum(np.array(dog_breed_predictions)==np.argmax(targets_test, axis=1))/len(dog_breed_predictions)
 print('Test accuracy: %.4f%%' % test_accuracy)
 
 
