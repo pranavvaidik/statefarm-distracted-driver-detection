aa49127408bfba063c903c4d1d1d71ad31e014fe
Pranav Vaidik Dhulipala <pranav.vaidik@gmail.com>
2018-12-15 08:16:58 -0800

    cleared more bugs

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ from tqdm import tqdm
 
 def path_to_tensor(img_path):
 	# loads RGB image as PIL.Image.Image type
-	img = image.load_img(img_path, target_size=(224, 224))
+	img = image.load_img(img_path, target_size=(256, 256))
 	# convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
 	x = image.img_to_array(img)
 	# convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
