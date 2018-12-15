de5c3e4c40441cf7d4f99be4aa857b86600a8518
Pranav Vaidik Dhulipala <pranav.vaidik@gmail.com>
2018-12-14 21:46:10 -0800

    Moved code to a python file from the jupyter notebook

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from keras.utils import np_utils
 import numpy as np
 from glob import glob
+from data_gen import DataGenerator
 
 #defining a function to load the dataset
 def load_dataset(path):
@@ -13,7 +14,7 @@ def load_dataset(path):
 
 #loading the datasets
 #change the directory in the github as well
-images, targets = load_dataset('../input/state-farm-distracted-driver-detection/train/')
+images, targets = load_dataset('imgs/train/')
 
 #splitting data to train, test and validation datasets
 images_train, images_rest, targets_train, targets_rest = train_test_split( images, targets, train_size=0.8, random_state=42)
@@ -30,14 +31,80 @@ print('There are %d number of test images' % len(images_test))
 
 
 import os
-print(os.listdir("../input/state-farm-distracted-driver-detection/train"))
+print(os.listdir("imgs/train"))
+
+################################################################
+
+# Parameters for the Data Generator
+params = {'batch_size' : 64,
+          'shuffle': True}
+
+# creating generators
+training_generator = DataGenerator(images_train, targets_train, **params)
+validation_generator = DataGenerator(images_val, targets_val, **params)
+testing_generator = DataGenerator(images_test, targets_test, **params)
+
+################################################################
+
+
+# Constructing Vanilla model
+
+from  keras.layers  import Conv2D, MaxPooling2D, GlobalAveragePooling2D
+from keras.layers import Dropout, Flatten, Dense
+from keras.models import Sequential
+
+model = Sequential()
+
+### TODO: Define your architecture.
+
+model.add(Conv2D(filters = 32, kernel_size = 3, strides = 1, padding = 'same', activation = None, input_shape = (256,256,3)))
+#model.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='same'))
+#model.add(Conv2D(filters = 64, kernel_size = 3, strides = 1, padding = 'same', activation = None))
+#model.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='same'))
+#model.add(Conv2D(filters = 128, kernel_size = 3, strides = 1, padding = 'same', activation = 'relu'))
+#model.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='same'))
+#model.add(Conv2D(filters = 256, kernel_size = 3, strides = 2, padding = 'same', activation = 'relu'))
+model.add(GlobalAveragePooling2D())
+
+#model.add(Dropout(0.5))
+
+model.add(Dense(10,activation = 'softmax'))
+
+model.summary()
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
 
 
 
 
 
-###########################################################
+# Training the vanilla model
+
+from keras.callbacks import ModelCheckpoint  
+
+### TODO: specify the number of epochs that you would like to use to train the model.
+
+epochs = 25
+
+### .
+
+checkpointer = ModelCheckpoint(filepath='weights.best.from_scratch.hdf5', 
+                               verbose=1, save_best_only=True)
+
+model.fit_generator(generator=training_generator, validation_data=validation_generator, epochs= epochs, callbacks=[checkpointer], use_multiprocessing=True, workers=6)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
