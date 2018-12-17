127e1c381e1433efd96f286e834144076aac09a2
Pranav Vaidik Dhulipala <pranav.vaidik@gmail.com>
2018-12-17 00:29:33 -0800

    Able to train the model, but with low accuracy. added more print statements for debugging

--- vgg_train.py
+++ vgg_train.py
@@ -130,7 +130,7 @@ from keras.callbacks import ModelCheckpoint
 
 ### TODO: specify the number of epochs that you would like to use to train the model.
 
-epochs = 25
+epochs = 10
 
 ###
 
@@ -147,8 +147,16 @@ top_model.fit(bottleneck_features_train, targets_train,
 #load model with best validation loss
 top_model.load_weights('weights.best.from_scratch.vgg16.hdf5')
 
+val_pred = [np.argmax(top_model.predict(np.expand_dims(tensor, axis=0))) for tensor in bottleneck_features_val]
+
+
 
 predictions = [np.argmax(top_model.predict(np.expand_dims(tensor, axis=0))) for tensor in bottleneck_features_test]
+
+
+val_accuracy = 100*np.sum(np.array(val_pred)==np.argmax(val_pred, axis=1))/len(predictions)
+print('Validation accuracy: %.4f%%' % val_accuracy)
+
 
 # report test accuracy
 test_accuracy = 100*np.sum(np.array(predictions)==np.argmax(targets_test, axis=1))/len(predictions)
