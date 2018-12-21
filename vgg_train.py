bf00349244ffb3fec7b088cc1e51d34329ebeba3
Pranav Vaidik Dhulipala <pranav.vaidik@gmail.com>
2018-12-20 21:49:53 -0800

    Put the "splitting the dataset" part in a function so that its easier to call

--- vgg_train.py
+++ vgg_train.py
@@ -7,31 +7,41 @@ from data_gen import DataGenerator
 
 #defining a function to load the dataset
 def load_dataset(path):
-    data = load_files(path)
-    driver_images = np.array(data['filenames'])
-    driver_activities = np_utils.to_categorical(np.array(data['target']))
-    return driver_images, driver_activities
-
-#loading the datasets
-#change the directory in the github as well
-images, targets = load_dataset('imgs/train/')
-
-#splitting data to train, test and validation datasets
-images_train, images_rest, targets_train, targets_rest = train_test_split( images, targets, train_size=0.8, random_state=42)
-images_val, images_test, targets_val, targets_test = train_test_split( images_rest, targets_rest, train_size=0.5, random_state=42)
+	data = load_files(path)
+	driver_images = np.array(data['filenames'])
+	driver_activities = np_utils.to_categorical(np.array(data['target']))
+	return driver_images, driver_activities
 
 
-#printing the dataset statistics
-print('There are %d total number of driver images' % len(images))
 
 
-print('There are %d number of train images' % len(images_train))
-print('There are %d number of validation images' % len(images_val))
-print('There are %d number of test images' % len(images_test))
+def build_training_sets():
 
 
-import os
-print(os.listdir("imgs/train"))
+
+	#loading the datasets
+	#change the directory in the github as well
+	images, targets = load_dataset('imgs/train/')
+
+	#splitting data to train, test and validation datasets
+	images_train, images_rest, targets_train, targets_rest = train_test_split( images, targets, train_size=0.8, random_state=42)
+	images_val, images_test, targets_val, targets_test = train_test_split( images_rest, targets_rest, train_size=0.5, random_state=42)
+
+
+	#printing the dataset statistics
+	print('There are %d total number of driver images' % len(images))
+
+
+	print('There are %d number of train images' % len(images_train))
+	print('There are %d number of validation images' % len(images_val))
+	print('There are %d number of test images' % len(images_test))
+
+
+	#import os
+	#print(os.listdir("imgs/train"))
+
+	return images_train images_test, images_val, targets_train, targets_test, targets_val
+
 
 ################################################################
 
@@ -57,6 +67,10 @@ def paths_to_tensor(img_paths):
 
 from PIL import ImageFile                            
 ImageFile.LOAD_TRUNCATED_IMAGES = True                 
+
+
+images_train images_test, images_val, targets_train, targets_test, targets_val = build_training_sets()
+print(" Loaded the train, test and validation sets successfully!! ")
 
 # pre-process the data for Keras
 train_tensors = paths_to_tensor(images_train).astype('float32')/255
