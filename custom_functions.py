339d386e5870c1f9c5b0a7353b6e94f89672fc78
Pranav Vaidik Dhulipala <pranav.vaidik@gmail.com>
2018-12-24 15:17:56 -0800

    pushed the bottleneck feature generation part to the custom functions file, which generates the features only when it cannot find the data files

--- custom_functions.py
+++ custom_functions.py
@@ -53,3 +53,57 @@ def path_to_tensor(img_path):
 def paths_to_tensor(img_paths):
 	list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
 	return np.vstack(list_of_tensors)
+	
+	
+def generate_bottleneck_features():
+	images_train, images_test, images_val, targets_train, targets_test, targets_val = build_training_sets()
+	print(" Loaded the train, test and validation sets successfully!! ")
+
+	# pre-process the data for Keras
+	train_tensors = paths_to_tensor(images_train).astype('float32')/255
+	valid_tensors = paths_to_tensor(images_val).astype('float32')/255
+	test_tensors = paths_to_tensor(images_test).astype('float32')/255
+
+
+	datagen = ImageDataGenerator(
+	    rotation_range=20,
+	    width_shift_range=0.2,
+	    height_shift_range=0.2,
+	    zoom_range=0.2,
+	    horizontal_flip=False)
+	    
+	train_gen = datagen.flow(train_tensors, targets_train, batch_size = 128)
+	test_gen = datagen.flow(test_tensors, targets_test, batch_size = 128)
+	validation_gen = datagen.flow(valid_tensors, targets_val, batch_size = 128)
+
+	# define VGG16 model
+	vgg_model = VGG16(weights='imagenet', include_top = False)
+
+	vgg_model.summary()
+
+	vgg_layers = [l for l in vgg_model.layers]
+
+	trimmed_model = Sequential()
+
+	for layer in vgg_layers[:11]:
+		layer.trainable = False
+		trimmed_model.add(layer)
+
+	print("Trimmed")
+	trimmed_model.summary()
+	trimmed_model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+	# generating bottleneck features as generator objetcs, steps are based on the  number of tensors in each set and also the data augmentation
+	bottleneck_features_train = trimmed_model.predict_generator(train_gen, steps = 150)
+	bottleneck_features_validation = trimmed_model.predict_generator(validation_gen, steps = 20)
+	bottleneck_features_test = trimmed_model.predict_generator(test_gen, steps = 20)
+
+
+
+
+	np.save('bottleneck_features_train.npy', bottleneck_features_train)
+	np.save('bottleneck_features_validation.npy' bottleneck_features_validation)
+	np.save('bottleneck_features_test.npy', bottleneck_features_test)
+	
+	return bottleneck_features_train, bottleneck_features_validation, bottleneck_features_test
+
