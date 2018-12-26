b777f130bf17a36d52887a3ad1b04761fb340b15
Pranav Vaidik Dhulipala <pranav.vaidik@gmail.com>
2018-12-26 13:08:37 -0800

    still having memory errors. Now added the top layers directly to the vgg model. This eliminates the need to store bottleneck features. However, it should increase the training time by a lot

--- custom_functions.py
+++ custom_functions.py
@@ -10,6 +10,7 @@ from keras.models import Sequential
 from keras.layers import Dropout, Flatten, Dense
 from keras.preprocessing import image                  
 from tqdm import tqdm
+import keras
 
 #defining a function to load the dataset
 def load_dataset(path):
@@ -17,6 +18,64 @@ def load_dataset(path):
 	driver_images = np.array(data['filenames'])
 	driver_activities = np_utils.to_categorical(np.array(data['target']))
 	return driver_images, driver_activities
+
+
+
+class DataGenerator(keras.utils.Sequence):
+
+	#def __init__(self, list_file_paths, labels, batch_size=32, dim=(32,32,32), n_channels=1,
+	#             n_classes=10, shuffle=True):
+	def __init__(self, list_file_paths, labels, batch_size=32, shuffle=True):
+
+		#Initialization
+		self.batch_size = batch_size
+		self.labels = labels
+		self.list_file_paths = list_file_paths
+		self.shuffle = shuffle
+		self.on_epoch_end()
+
+	def __len__(self):
+		#Denotes the number of batches per epoch
+		return int(np.floor(len(self.list_file_paths) / self.batch_size))
+
+	def __getitem__(self, index):
+		#Generate one batch of data
+		# Generate indexes of the batch
+		indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+		# Find list of IDs
+		list_IDs_temp = [self.list_file_paths[k] for k in indexes]
+
+		# Generate data
+		X = self.__data_generation(list_IDs_temp)
+		y = self.labels[indexes]
+
+		return X, y
+
+	def on_epoch_end(self):
+		#Updates indexes after each epoch
+		self.indexes = np.arange(len(self.list_file_paths))
+		if self.shuffle == True:
+	    		np.random.shuffle(self.indexes)
+
+	def path_to_tensor(self, path):
+		img = image.load_img(path, target_size=(256, 256))
+		x = image.img_to_array(img)
+		return np.expand_dims(x, axis=0)
+
+	#define a function that reads a path to an image and returns a tensor suitable for keras
+	#def paths_to_tensor(paths_list):
+	#    img_list = [path_to_tensor(img_path) for img_path in tqdm(paths_list)]
+	#    return np.vstack(img_list)
+
+	def __data_generation(self, list_file_paths_temp):
+
+		#Generates data containing batch_size samples
+		# X : (n_samples, *dim, n_channels)
+		# Initialization
+		img_list = [self.path_to_tensor(img_path) for img_path in list_file_paths_temp]
+		return np.vstack(img_list).astype('float32')/255
+
 
 
 
@@ -81,7 +140,7 @@ def generate_bottleneck_features():
 	validation_gen = datagen.flow(valid_tensors, targets_val, batch_size = 128)
 
 	# define VGG16 model
-	vgg_model = VGG16(weights='imagenet', include_top = False)
+	vgg_model = VGG16(weights='imagenet', include_top = False, input_shape = (256,256,3))
 
 	vgg_model.summary()
 
@@ -97,17 +156,17 @@ def generate_bottleneck_features():
 	trimmed_model.summary()
 	trimmed_model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
-	# generating bottleneck features as generator objetcs, steps are based on the  number of tensors in each set and also the data augmentation
-	bottleneck_features_train = trimmed_model.predict_generator(train_gen, steps = 150)
-	bottleneck_features_validation = trimmed_model.predict_generator(validation_gen, steps = 20)
-	bottleneck_features_test = trimmed_model.predict_generator(test_gen, steps = 20)
+	## generating bottleneck features as generator objetcs, steps are based on the  number of tensors in each set and also the data augmentation
+	#bottleneck_features_train = trimmed_model.predict_generator(train_gen, steps = 150)
+	#bottleneck_features_validation = trimmed_model.predict_generator(validation_gen, steps = 20)
+	#bottleneck_features_test = trimmed_model.predict_generator(test_gen, steps = 20)
 
 
 
 
-	np.save('bottleneck_features_train.npy', bottleneck_features_train)
-	np.save('bottleneck_features_validation.npy' bottleneck_features_validation)
-	np.save('bottleneck_features_test.npy', bottleneck_features_test)
+	#np.save('bottleneck_features_train.npy', bottleneck_features_train)
+	#np.save('bottleneck_features_validation.npy', bottleneck_features_validation)
+	#np.save('bottleneck_features_test.npy', bottleneck_features_test)
 	
-	return bottleneck_features_train, bottleneck_features_validation, bottleneck_features_test
+	return vgg_model# bottleneck_features_train, bottleneck_features_validation, bottleneck_features_test
 
--- test.py
+++ test.py
@@ -8,8 +8,64 @@ from tqdm import tqdm
 import numpy as np
 
 
-bottleneck_features_train, bottleneck_features_validation, bottleneck_features_test = generate_bottleneck_features()
 
+
+
+
+
+images_train, images_test, images_val, targets_train, targets_test, targets_val = build_training_sets()
+print(" Loaded the train, test and validation sets successfully!! ")
+
+# pre-process the data for Keras
+train_tensors = paths_to_tensor(images_train).astype('float32')/255
+valid_tensors = paths_to_tensor(images_val).astype('float32')/255
+test_tensors = paths_to_tensor(images_test).astype('float32')/255
+
+
+datagen = ImageDataGenerator(
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=False)
+    
+train_gen = datagen.flow(train_tensors, targets_train, batch_size = 128)
+test_gen = datagen.flow(test_tensors, targets_test, batch_size = 128)
+validation_gen = datagen.flow(valid_tensors, targets_val, batch_size = 128)
+
+# define VGG16 model
+vgg_model = VGG16(weights='imagenet', include_top = False, input_shape = (256,256,3))
+
+#vgg_model.summary()
+
+vgg_layers = [l for l in vgg_model.layers]
+
+model = Sequential()
+
+for layer in vgg_layers[:11]:
+	layer.trainable = False
+	model.add(layer)
+
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+"""
+
+try:
+	print("Attempting to load the bottleneck features")
+	bottleneck_features_train = np.load('bottleneck_features_train.npy')
+	bottleneck_features_validation = np.load('bottleneck_features_validation.npy')
+	bottleneck_features_test = np.load('bottleneck_features_test.npy')
+	print("Bottleeck features loaded successfully!!")
+except:
+	print("Bottleneck features were not found!")
+	bottleneck_features_train, bottleneck_features_validation, bottleneck_features_test = generate_bottleneck_features()
+"""
 
 
 """
@@ -25,6 +81,7 @@ top_model.add(Dense(10, activation='sigmoid'))
 top_model.summary()
 
 top_model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+"""
 
 # train the top layers
 
@@ -42,24 +99,30 @@ epochs = 10
 checkpointer = ModelCheckpoint(filepath='weights.best.from_scratch.vgg16.hdf5', 
                                verbose=1, save_best_only=True)
 
-#model.fit_generator(generator=training_generator, validation_data=validation_generator, epochs= epochs, callbacks=[checkpointer], use_multiprocessing=True, workers=6)
+model.fit_generator(generator=train_gen, steps_per_epoch=300, validation_data=validation_gen, validation_steps = 40, epochs= epochs, callbacks=[checkpointer], use_multiprocessing=True, workers=6, verbose=1)
 
 #direct_train
-top_model.fit(bottleneck_features_train, targets_train, 
-          validation_data=(bottleneck_features_val, targets_val),
-          epochs=epochs, batch_size=20, callbacks=[checkpointer], verbose=1)
+#model.fit(bottleneck_features_train, targets_train, validation_data=(bottleneck_features_val, targets_val), epochs=epochs, batch_size=20, callbacks=[checkpointer], verbose=1)
 
 #load model with best validation loss
-top_model.load_weights('weights.best.from_scratch.vgg16.hdf5')
-
-val_pred = [np.argmax(top_model.predict(np.expand_dims(tensor, axis=0))) for tensor in bottleneck_features_val]
+model.load_weights('weights.best.from_scratch.vgg16.hdf5')
 
 
+valid_acc = model.evaluate_generator(generator = validation_gen, steps=None, max_queue_size=10, workers=1, use_multiprocessing=False, verbose=1)
+test_accuracy = model.evaluate_generator(generator = test_gen, steps=None, max_queue_size=10, workers=1, use_multiprocessing=False, verbose=1)
 
-predictions = [np.argmax(top_model.predict(np.expand_dims(tensor, axis=0))) for tensor in bottleneck_features_test]
 
 
-val_accuracy = 100*np.sum(np.array(val_pred)==np.argmax(targets_val, axis=1))/len(val_pred)
-print('Validation accuracy: %.4f%%' % val_accuracy)
 
-"""
+#val_pred = [np.argmax(model.predict(np.expand_dims(tensor, axis=0))) for tensor in bottleneck_features_val]
+
+
+
+#predictions = [np.argmax(model.predict(np.expand_dims(tensor, axis=0))) for tensor in bottleneck_features_test]
+
+
+#val_accuracy = 100*np.sum(np.array(val_pred)==np.argmax(targets_val, axis=1))/len(val_pred)
+print('Validation accuracy: %.4f%%' % valid_acc)
+print('Test accuracy: %.4f%%' % test_accuracy)
+
+
